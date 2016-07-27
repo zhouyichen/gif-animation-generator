1510a5cc1486ff713a20d7d97869456a3316150c
Zhou Yichen <zhouyichen123@gmail.com>
2016-07-27 15:57:37 +0800

    added html for input

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
 $(function() {
-	generateGIF("box", "手动点赞.jpg", 150, 52);
+	$('#generate').mouseup(function (){
+        var text = $('#inputString').val();
+        if (!text) {
+        	$('#inputStringForm').addClass('has-error');
+        	return;
+        } else {
+        	$('#inputStringForm').remove('has-error');
+        }
+        
+        var delay = parseInt($('#delay').val());
+        if (!delay) {
+        	delay = 150;
+        } else if (delay < 0) {
+        	delay = 100;
+        } else if (delay > 10000) {
+        	delay = 10000;
+        } else {
+        	$('#delayForm').addClass('has-error');
+        }
+
+        generateGIF(text, delay, 45);
+    });
 });
