33ec2e2c3e48d3e64a8418be0e1d1d1c811ee610
Zhou Yichen <zhouyichen123@gmail.com>
2016-07-28 11:13:42 +0800

    refactor and add cursor effect

--- js/generator.js
+++ js/generator.js
@@ -3,19 +3,11 @@ function generateGIF(inputString, delay, size) {
 	var y_offset = size * 1.5;
 	var x_offset = size * 0.6;
 
-	var c = document.createElement("canvas");
-	var ctx = c.getContext("2d");
 	var str = "";
 	var encoder = new GIFEncoder();
 	encoder.setRepeat(0);
 	encoder.setDelay(delay);
 	encoder.start();
-
-	// setup size and background
-	ctx.font = font_setting;
-	var full_length = ctx.measureText(inputString).width + 2 * x_offset;
-	c.setAttribute("height", size * 2.5);
-	c.setAttribute("width", full_length);
 
 	add_text_frame(inputString);
 	add_text_frame('');
@@ -34,6 +26,15 @@ function generateGIF(inputString, delay, size) {
 	document.getElementById('result').src = data_url;
 
 	function add_text_frame(str) {
+		var c = document.createElement("canvas");
+		var ctx = c.getContext("2d");
+
+		// setup size and background
+		ctx.font = font_setting;
+		var full_length = ctx.measureText(inputString).width + 2 * x_offset;
+		c.setAttribute("height", size * 2.5);
+		c.setAttribute("width", full_length);
+
 		ctx.fillStyle = 'rgb(255,255,255)';
 	  	ctx.fillRect(0, 0, full_length, size * 2.5);
 
@@ -42,7 +43,17 @@ function generateGIF(inputString, delay, size) {
 		ctx.font = font_setting;
 
 		ctx.fillText(str, x_offset, y_offset);
+		var text_length = ctx.measureText(str).width + x_offset;
+
+		ctx.lineWidth = size / 12;
+		ctx.strokeStyle = "rgb(31,64,255)";
+		ctx.moveTo(text_length, y_offset * 1.2);
+		ctx.lineTo(text_length, y_offset - size);
+		ctx.stroke();
+
 		encoder.addFrame(ctx);
+		ctx.fillStyle = 'rgb(255,255,255)';
+	  	ctx.fillRect(0, 0, full_length, size * 2.5);
 	}
 }
 
